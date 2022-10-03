8b19c4a8989e80b263bb4301c90b94958b19387c
Vika <mandarinkavikabakalova@gmail.com>
2022-10-03 16:11:35 +0400

    Зачада 64

---
+++ Z64/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+//числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int b=1;
+string numbers (int n, int b)
+{
+    if (n <= b) return numbers (n+1,b)+ $"{n} ";
+    else return String.Empty;
+}
+Console.WriteLine(numbers(b,n));
